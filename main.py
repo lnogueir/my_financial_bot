0e13698a79975618799e9abd6e1d4c941bced93a
Lucas Nogueira <lnogueir@uwaterloo.ca>
2019-09-21 19:16:56 -0400

    change config file

--- main.py
+++ main.py
@@ -1,13 +1,18 @@
 from exceler import Exceler
-import webbrowser, os
+import webbrowser, os, time
 
 
 if __name__ == '__main__':
 	workbook_name = 'finances.xlsx'
-	exceler = Exceler(workbook_name)
-	exceler.read_workbook_to_python()
-	exceler.write_workbook_to_excel()
-	webbrowser.open('file://' + os.path.realpath(workbook_name))
+	five_hours = 60*60*5
+	try:
+		while True:
+			exceler = Exceler(workbook_name)
+			exceler.read_workbook_to_python()
+			exceler.write_workbook_to_excel()
+			time.sleep(five_hours)
+	except:	# opens your excel file when you kill the program	
+		webbrowser.open('file://' + os.path.realpath(workbook_name))
 
 
 
