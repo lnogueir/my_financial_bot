0199877405f24efb6bbafc0032004638a4607683
Lucas Nogueira <lnogueir@uwaterloo.ca>
2019-09-22 00:53:01 -0400

    bla

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ if __name__ == '__main__':
 	workbook_name = 'finances.xlsx'		
 	five_hours = 60*60*5
 	try:
-		while True
+		while True:
 			exceler = Exceler(workbook_name)
 			exceler.read_workbook_to_python()
 			exceler.write_workbook_to_excel()
